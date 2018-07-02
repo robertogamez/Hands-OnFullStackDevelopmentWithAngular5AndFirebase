3dffae99ec97ab70aecd9f0af766ba777d939296
Roberto Manuel Balderas Gamez <vn00m8d@mx.wal-mart.com>
2018-07-02 12:02:07 -0500

    Se añade soporte para dialogo modal

--- FriendsApp/friends/src/app/edit-dialog/edit-dialog.component.ts
+++ FriendsApp/friends/src/app/edit-dialog/edit-dialog.component.ts
@@ -1,15 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { AuthenticationService } from '../services/authentication.service';
+import { UserService } from '../services/user.service';
+import { User } from '../services/user';
+import { EditDetails, EditType } from './edit-details';
 
 @Component({
-  selector: 'app-edit-dialog',
-  templateUrl: './edit-dialog.component.html',
-  styleUrls: ['./edit-dialog.component.scss']
+    selector: 'app-edit-dialog',
+    templateUrl: './edit-dialog.component.html',
+    styleUrls: ['./edit-dialog.component.scss']
 })
 export class EditDialogComponent implements OnInit {
 
-  constructor() { }
+    isVisible: boolean;
+    titleMessage: string;
+    bodyTitle: string;
+    bodyLabel: string;
+    editType: EditType;
+    editDetails: EditDetails;
 
-  ngOnInit() {
-  }
+    constructor(
+        private authService: AuthenticationService,
+        private userService: UserService
+    ) {
+        this.editDetails = new EditDetails(authService, userService);
+    }
+
+    public setTitle(title: string): EditDialogComponent {
+        this.titleMessage = title;
+        return this;
+    }
+
+    public setBodyTitle(bodyTitle: string): EditDialogComponent {
+        this.bodyTitle = bodyTitle;
+        return this;
+    }
+
+    public setBodyLabel(bodyLabel: string): EditDialogComponent {
+        this.bodyLabel = bodyLabel;
+        return this;
+    }
+
+    public setEditType(editType: EditType): EditDialogComponent {
+        this.editType = editType;
+        return this;
+    }
+
+    public show() {
+        this.isVisible = true;
+    }
+
+    public hide() {
+        this.isVisible = false;
+    }
+
+    private onSubmit(editFormData): void {
+        this.editDetails.edit(this.editType, editFormData.value.editValue);
+    }
+
+    ngOnInit() {
+    }
 
 }
+
--- FriendsApp/friends/src/app/services/authentication.service.ts
+++ FriendsApp/friends/src/app/services/authentication.service.ts
@@ -37,4 +37,12 @@ export class AuthenticationService {
         return this.angularFireAuth.auth.signOut();
     }
 
+    public changeEmail(email: string): Promise<any> {
+        return this.angularFireAuth.auth.currentUser.updateEmail(email);
+    }
+
+    public changePassword(password: string): Promise<any> {
+        return this.angularFireAuth.auth.currentUser.updatePassword(password);
+    }
+
 }
